71002eadf1f2531fbd9597dd12235e8c3e8fc06a
mboyapatroba <mboyapatroba1@gmail.com>
2022-08-01 23:22:47 +0300

    first commit

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+
+<?php
+
+
+$db_connection = mysqli_connect("localhost","root","","login_rregistration");
+// Check connection
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
